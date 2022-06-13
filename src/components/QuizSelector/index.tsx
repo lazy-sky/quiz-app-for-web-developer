ba35543d056adc5005f06194cd704e7a4021552a
lazy-sky <1cielo@naver.com>
2022-06-14 02:52:34 +0900

    Fix: Edit route path in QuizSelector

--- src/components/QuizSelector/index.tsx
+++ src/components/QuizSelector/index.tsx
@@ -6,19 +6,19 @@ const QuizSelector = () => {
   return (
     <ul className={styles.quizSelector}>
       <li>
-        <Link to='quiz/a'>
+        <Link to='/quiz/a'>
           <h3>Quiz A</h3>
           <p>Quiz A에 대한 설명</p>
         </Link>
       </li>
       <li>
-        <Link to='quiz/b'>
+        <Link to='/quiz/b'>
           <h3>Quiz B</h3>
           <p>Quiz B에 대한 설명</p>
         </Link>
       </li>
       <li>
-        <Link to='quiz/c'>
+        <Link to='/quiz/c'>
           <h3>Quiz C</h3>
           <p>Quiz C에 대한 설명</p>
         </Link>
