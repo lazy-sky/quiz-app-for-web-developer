7ad18632159ac9975d47dc632358d19c5f12cae4
lazy-sky <1cielo@naver.com>
2022-06-13 14:36:45 +0900

    Asset: Upload images for GNB

---
+++ src/assets/index.ts
@@ -0,0 +1,3 @@
+export { ReactComponent as HomeIcon } from './icon-home.svg'
+export { ReactComponent as PencilIcon } from './icon-pencil.svg'
+export { ReactComponent as PersonIcon } from './icon-person.svg'
