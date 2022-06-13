5043ebdc0aba1a0a1d1a5525c9a3c6a3e2c49e16
lazy-sky <1cielo@naver.com>
2022-06-14 02:39:55 +0900

    Feat: Make QuizSelector

---
+++ src/components/QuizSelector/index.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+
+import styles from './quizSelector.module.scss'
+
+const QuizSelector = () => {
+  return (
+    <ul className={styles.quizSelector}>
+      <li>
+        <Link to='quiz/a'>
+          <h3>Quiz A</h3>
+          <p>Quiz A에 대한 설명</p>
+        </Link>
+      </li>
+      <li>
+        <Link to='quiz/b'>
+          <h3>Quiz B</h3>
+          <p>Quiz B에 대한 설명</p>
+        </Link>
+      </li>
+      <li>
+        <Link to='quiz/c'>
+          <h3>Quiz C</h3>
+          <p>Quiz C에 대한 설명</p>
+        </Link>
+      </li>
+    </ul>
+  )
+}
+
+export default QuizSelector
