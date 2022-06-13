5d22905dd0641cc1c384bd4a83a90b61332a93a0
lazy-sky <1cielo@naver.com>
2022-06-14 01:39:00 +0900

    Feat: Make QuizContainer

---
+++ src/components/QuizContainer/index.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useRecoilState } from 'recoil'
+import { useMount } from 'react-use'
+import cx from 'classnames'
+
+import { IQuiz } from 'types/quiz'
+import { answerSheetState, quizsState } from 'store/atom'
+
+import styles from './quizContainer.module.scss'
+
+const QuizContainer = ({ currentQuizs }: { currentQuizs: IQuiz[] }) => {
+  const navigate = useNavigate()
+  const [quizs, setQuizs] = useRecoilState(quizsState)
+  const [answerSheet, setAnswerSheet] = useRecoilState(answerSheetState)
+  const [currentQuizIndex, setCurrentQuizIndex] = useState(0)
+
+  useMount(() => {
+    setQuizs(currentQuizs)
+    const initalized = Array.from({ length: quizs.length }).fill(-1) as number[]
+    setAnswerSheet(initalized)
+  })
+
+  const handleChoiceClick = (choiceIndex: number) => {
+    setAnswerSheet((prev) => prev.map((answer, index) => (index === currentQuizIndex ? choiceIndex : answer)))
+  }
+
+  const handlePrevClick = () => {
+    setCurrentQuizIndex((prev) => prev - 1)
+  }
+
+  const handleNextClick = () => {
+    setCurrentQuizIndex((prev) => prev + 1)
+  }
+
+  const handleShowResultClick = () => {
+    navigate('/result')
+  }
+
+  return (
+    <div className={styles.quizCard}>
+      <div className={styles.questionCount}>
+        문제 <span>{currentQuizIndex + 1}</span> / {quizs.length}
+      </div>
+      <h3 className={styles.question}>{quizs[currentQuizIndex]?.question}</h3>
+      <div className={styles.choices}>
+        {quizs[currentQuizIndex]?.choices.map((choice, index) => (
+          <button
+            type='button'
+            key={choice}
+            onClick={() => handleChoiceClick(index)}
+            className={cx(answerSheet[currentQuizIndex] === index && styles.isSelected)}
+          >
+            {choice}
+          </button>
+        ))}
+      </div>
+      <div className={styles.moveBtns}>
+        {currentQuizIndex > 0 && (
+          <button type='button' onClick={handlePrevClick}>
+            이전 문제
+          </button>
+        )}
+        {currentQuizIndex < quizs.length - 1 && (
+          <button type='button' onClick={handleNextClick}>
+            다음 문제
+          </button>
+        )}
+        {currentQuizIndex === quizs.length - 1 && (
+          <button type='button' onClick={handleShowResultClick} className={styles.result}>
+            결과 보기
+          </button>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default QuizContainer
